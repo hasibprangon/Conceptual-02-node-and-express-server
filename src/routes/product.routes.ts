2c1ec391e04fe5bdd955166e5d446d353464d96e
hasibprangon <hasibulhossainp09@gmail.com>
2025-06-22 16:32:06 +0600

    showing products route

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,10 +1,19 @@
 import { IncomingMessage, ServerResponse } from "http";
+import { handleProducts } from "../controller/products.controller";
 
-export const routeHandler = async(req: IncomingMessage, res: ServerResponse) => {
+export const routeHandler = async (req: IncomingMessage, res: ServerResponse) => {
     const url = req.url
     // console.log(req);
-    if(url ===  '/' && req.method === 'GET'){
-        res.writeHead(200, {'Content-type': 'text/plain'})
+    if (url === '/' && req.method === 'GET') {
+        res.writeHead(200, { 'Content-type': 'text/plain' })
         res.end('Welcome to nodejs server')
+    }
+    else if (url?.startsWith('/products')) {
+       await handleProducts(req, res)
+    }
+
+    else {
+        res.writeHead(404);
+        res.end('Route not found')
     }
 }
