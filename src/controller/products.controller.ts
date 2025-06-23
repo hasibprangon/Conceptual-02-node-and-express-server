c849cea3aa019e3044b2a70951f1d6316cc1d8ec
hasibprangon <hasibulhossainp09@gmail.com>
2025-06-23 20:27:00 +0600

    post method

--- src/controller/products.controller.ts
+++ src/controller/products.controller.ts
@@ -1,5 +1,7 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { products } from "../data/products";
+import { parseBody } from "../utils/parseBody";
+import { IProduct } from "../types/products";
 
 export const handleProducts = async(req: IncomingMessage, res: ServerResponse) => {
     const url = req.url;
@@ -7,5 +9,18 @@ export const handleProducts = async(req: IncomingMessage, res: ServerResponse) =
     if(url === '/products' && req.method === 'GET') {
         res.writeHead(200, {'content-type': 'application/json'});
         res.end(JSON.stringify(products))
+    }
+    else if(url === '/products' && req.method === 'POST'){
+        try{
+            // const body = await parseBody(req);
+            const newProduct: IProduct = {id: 3, name: 'book'};
+            products.push(newProduct);
+            res.writeHead(201, {'content-type': 'application/json'})
+            res.end(JSON.stringify(products))
+        }
+        catch(err){
+            res.writeHead(400);
+            res.end(`${err}`)
+        }
     }
 }
